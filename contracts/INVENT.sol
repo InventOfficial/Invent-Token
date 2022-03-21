d261597a3ffa92dc3c93736ddc4f104870682b59
InventOfficial <96603898+InventOfficial@users.noreply.github.com>
2022-03-21 07:12:35 -0700

    Update INVENT.sol

--- contracts/INVENT.sol
+++ contracts/INVENT.sol
@@ -491,13 +491,13 @@ contract INVENT is Context, IERC20, Ownable {
         _wallet_buyback = payable(_buybackWallet);
 
         _balance_reflected[owner()] = _supply_reflected;
+	
+	// Pancakeswap Router Initialization & Pair creation
+        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(PCSRouter);
+        uniswapV2Pair = IUniswapV2Factory(_uniswapV2Router.factory()).createPair(address(this), _uniswapV2Router.WETH());
+        uniswapV2Router = _uniswapV2Router;
         
        
-        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(PCSRouter);
-        uniswapV2Pair = PCSRouter;
-        uniswapV2Router = _uniswapV2Router;
-
-
         _isExcludedFromFee[owner()] = true;
         _isExcludedFromFee[address(this)] = true;
         _isExcludedFromFee[deadAddress] = true;
